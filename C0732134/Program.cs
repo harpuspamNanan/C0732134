608ec141adddc9d0d5a33a075b8581928933f2f8
hpsnannar@outlook.com <hpsnannar@outlook.com>
2019-01-18 21:32:13 -0500

    Working with Lists

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C0732134
 {
@@ -7,11 +8,24 @@ namespace C0732134
         static void Main(string[] args)
         {
             DataSet _Mary = new DataSet("Mary", 3.8, 20);
-            DataSet _John = new DataSet("John", 2.0, 10);
+            DataSet _John = new DataSet("John", 2.00, 10);
             DataSet _Mark = new DataSet("Mark", 0.2, 3);
             DataSet _Steve = new DataSet("Mary", 2.9, 16);
             DataSet _Cindy = new DataSet("Cindy", 3.3, 18);
 
+            var StudentList = new List<DataSet>();
+            StudentList.Add(_Mary);
+            StudentList.Add(_John);
+            StudentList.Add(_Mark);
+            StudentList.Add(_Steve);
+            StudentList.Add(_Cindy);
+
+            Console.WriteLine("\tName \tGPA \tAtendance\n");
+            foreach (var student in StudentList)
+            {
+                Console.WriteLine("\t"+student.name +"\t"+student.gpa+"\t"+student.Attendance);
+            }
+            Console.ReadLine();
         }
 
         class DataSet
@@ -25,6 +39,14 @@ namespace C0732134
                 name = name_a;
                 gpa = gpa_a;
                 Attendance = Attendance_a;
+            }
+        }
+
+        class StudentList
+        {
+            public void CICS()
+            {
+                
             }
         }
     }
