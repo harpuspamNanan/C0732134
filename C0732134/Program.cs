82e83a40706081f7327366e2e214487ee3ea826d
hpsnannar@outlook.com <hpsnannar@outlook.com>
2019-01-18 20:38:24 -0500

    5 Values Entered.

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -6,7 +6,26 @@ namespace C0732134
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataSet _Mary = new DataSet("Mary", 3.8, 20);
+            DataSet _John = new DataSet("John", 2.0, 10);
+            DataSet _Mark = new DataSet("Mark", 0.2, 3);
+            DataSet _Steve = new DataSet("Mary", 2.9, 16);
+            DataSet _Cindy = new DataSet("Cindy", 3.3, 18);
+
+        }
+
+        class DataSet
+        {
+            public string name;
+            public double gpa ;
+            public int Attendance ;
+
+            public DataSet(string name_a, double gpa_a ,int Attendance_a)
+            {
+                name = name_a;
+                gpa = gpa_a;
+                Attendance = Attendance_a;
+            }
         }
     }
 }
